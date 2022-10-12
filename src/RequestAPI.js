62d3005327e65170c4119fca458efaa7d3c6cfef
razvanccst <razvanccst@gmail.com>
2022-10-12 17:26:04 +0300

    Fetch API & header

---
+++ src/RequestAPI.js
@@ -0,0 +1,11 @@
+const key = 'fbfddd414f80abc3155bc006be0e1a2c';
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
