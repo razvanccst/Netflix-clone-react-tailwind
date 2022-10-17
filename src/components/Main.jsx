6ce0b9a22d32e2b74ac6d40044c7801fba79eb88
razvanccst <48159550+razvanccst@users.noreply.github.com>
2022-10-17 16:07:41 +0300

    Frontend added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,6 +13,7 @@ const Main = () => {
 
   const movie = movies[Math.floor(Math.random() * movies.length)];
 
+
   return (
     <div className="w-full h-[550px] text-white">
       <div className="w-full h-full">
@@ -22,8 +23,9 @@ const Main = () => {
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
+
         <div className="absolute w-full top-[20%] p-4 md:p-8">
-            <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
           <div className="my-4">
             <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
               Play
